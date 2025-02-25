a0db7c0a8fb78c93e1da00bf79be407fb3c2d621
Nestor Acuna Blanco <nestor.acuna@ibm.com>
2025-02-25 12:41:34 +0100

    fix: use only http.server for simplicity

--- python-server-demo.py
+++ python-server-demo.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+import http.server
+import socketserver
 
-app = Flask(__name__)
+PORT = 8080
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)  # Send HTTP status code 200 (OK)
+        self.send_header("Content-type", "text/html")  # Set the content type
+        self.end_headers()  # End the headers
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        # Write the response body
+        self.wfile.write(b"<html><body>")
+        self.wfile.write(b"<h1>Openshift Virt Demo</h1>")
+        self.wfile.write(b"<p>This is a demo web server</p>")
+        self.wfile.write(b"</body></html>")
+
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
+
